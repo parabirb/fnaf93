d76a5e042522446ad18782e710bdebbfeb9033cf
parabirb <parabirb@protonmail.ch>
2021-09-23 22:12:51 -0500

    Add fnaf93

---
+++ fnaf93.js
@@ -0,0 +1,3 @@
+(function fnaf93() {
+    $window({ url: "https://parabirb.github.io/FNAF-JS/feed.html", title: "FNAF-JS", width: 640, height: 480, help: "FNAF, ported to JS by Slynchy", automaximize: true });
+})
